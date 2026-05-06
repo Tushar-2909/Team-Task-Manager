75a9003b66ef87e04d1b205a129bd0b186014987
Tushar <tushargandhwani@gmail.com>
2026-05-07 02:36:54 +0530

    fix migrations for accounts_user

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,8 @@
 set -o errexit
 
 pip install -r requirements.txt
+python manage.py makemigrations accounts
 python manage.py makemigrations
+
 python manage.py migrate
 python manage.py collectstatic --noinput
